65f96ec504375cde78ced31fed0bce70950ddc69
Amogh <badugu.amogh@gmail.com>
2022-04-13 18:10:40 -0400

    dropdown control + calls to component_builders

--- viz/static/js/viz.js
+++ viz/static/js/viz.js
@@ -3,6 +3,10 @@ console.log('viz.js loaded');
 getNftCollection = function() {
     dropdown = document.getElementById('nftCollection');
     console.log(dropdown.value);
+    d3.select("#components").selectAll("*").remove();
+    if( dropdown.value == ""){
+        return;
+    }
     const request = new XMLHttpRequest();
     request.open('GET', '/data/get_all/' + dropdown.value, true);
     request.addEventListener('load', function() {
@@ -10,6 +14,14 @@ getNftCollection = function() {
             // Success!
             const data = JSON.parse(request.responseText);
             console.log(data);
+            //remove previous chart
+            
+            // update the chart
+            buildPriceVolumeChart(data.price_data);
+            buildFeatureChart(data.feature_data);
+            buildGraph(data.graph_data);
+            buildSentimentChart(data.sentiment_data);
+            buildWordCloud(data.word_cloud_data);
         } else {
             // We reached our target server, but it returned an error
             console.log('error');
@@ -17,4 +29,6 @@ getNftCollection = function() {
     }
     );
     request.send();
+
+    
 }
