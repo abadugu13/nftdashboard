c4677bf9ae2198a30f2f7ace87631bad034c230e
tejajuttu <shaney.dota@gmail.com>
2022-04-24 18:29:41 -0400

    added more views

--- viz/views.py
+++ viz/views.py
@@ -6,12 +6,18 @@ def index(request):
     context = {
         'title': 'NFT Dashboard',
         "collections":[
+            {"name":"Alien Worlds", "id": "alien.worlds"},
+            {"name":"Axie NFT", "id": "axie"},
             {"name": "CryptoKitties", "id": "cryptokitties"},
             {"name": "CryptoPunks", "id": "cryptopunks"},
-            {"name": "Azuki", "id": "azuki"},
             {"name": "Cryptovoxels", "id": "cryptovoxels"},
-            {"name": "CyberKongz", "id": "cyberkongz"},
             {"name":"Decentraland", "id": "decentraland"},
+            {"name":"Hashmasks", "id": "hashmasks"},
+            {"name":"Rarible", "id": "rarible"},
+            {"name":"Sorare", "id": "sorare"}
+            # {"name": "CyberKongz", "id": "cyberkongz"},
+            # {"name": "Azuki", "id": "azuki"},
+
         ]
     }
     return render(request, 'index.html', context)
